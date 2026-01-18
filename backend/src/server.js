2d418281feef081bc04d3c589f07707848d7a8c9
ANSHSINGH050404 <114014895+ANSHSINGH050404@users.noreply.github.com>
2026-01-18 15:07:21 +0530

    deployment config edit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,44 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import { ENV } from "./config/env.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Load env vars
+dotenv.config();
 
 // Connect to database
 connectDB();
 
 const app = express();
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // Middlewares
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 
-// Basic route
-app.get("/", (req, res) => {
+// Routes
+app.use("/api/users", userRoutes);
+
+app.get("/api", (req, res) => {
   res.json({ message: "Welcome to Talent IQ API" });
 });
 
-// Routes
-app.use("/api/users", userRoutes);
+//make our app for production
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+  app.use((req, res) => {
+    res.sendFile(path.resolve(__dirname, "../../frontend/dist/index.html"));
+  });
+}
 
 // Port configuration
 const PORT = ENV.PORT || 5000;
